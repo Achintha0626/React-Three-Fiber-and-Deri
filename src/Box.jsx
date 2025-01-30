320a85f28bacd66b00143f5a89e3b45490ad5efb
Achintha0626 <124795790+Achintha0626@users.noreply.github.com>
2025-01-30 17:26:12 +0530

    learn about useFrame
    this is the one we use rotate the object we use
    
    useFrame provides two arguments:
    
    State → An object containing useful properties like camera, clock, and scene.
    Delta → The time difference between frames (helps with smooth animations).
    In your code, we don't need the state object, so we use _ to ignore it.
    
    This is just a coding convention to indicate "I don't need this value."
    
    ref.current.position.y=Math.sin(state.clock.getElapsedTime()*2)=This line makes the object move up and down
    *2 means speed the wave

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,9 +1,13 @@
+import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 
 export function Box(props) {
   const ref = useRef();
-  useEffect(() => {
-    console.log(ref);
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += 1 * delta;
+    ref.current.rotation.y += 0.5 * delta;
+    ref.current.position.y=Math.sin(state.clock.getElapsedTime()*2)
   });
 
   return (
